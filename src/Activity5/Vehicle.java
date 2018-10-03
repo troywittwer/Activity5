b158728f756aab6a50e0b48ffd853787f1ad5c16
twittwer9029 <twittwer9029@eagle.fgcu.edu>
2018-10-03 10:19:57 -0400

    Added comments to the Vehicle class for step 9.

--- src/Activity5/Vehicle.java
+++ src/Activity5/Vehicle.java
@@ -1,3 +1,13 @@
+/*
+This class implements two interfaces: Engine and Chassis. This means the class needs to overload
+every method listed under each interface. The instructions only requested private variables
+relating to the vehicle class, but also expects Chassis and Engine objects (one of each).
+
+Since one cannot create an instance of an interface, I set them to new instances of classes that
+implemented the interface. For example, the vehicleChassis object was declared as a Chassis object,
+but instantiated as a new VehicleChassis object. Likewise, the vehicleEngine object (declared as an
+Engine object) was instantiated as a new ManufacturedEngine object.
+ */
 package Activity5;
 
 import java.util.Date;
@@ -13,6 +23,9 @@ public class Vehicle implements Engine, Chassis {
   private String driveTrain;
   private Engine vehicleEngine;
 
+  /**
+   * A default constructor for the vehicle class. vehicleFrame and vehicleEngine receive
+   */
   Vehicle() {
     vehicleManufacturedDate = new Date();
     vehicleManufacturer = "Generic";
@@ -21,9 +34,27 @@ public class Vehicle implements Engine, Chassis {
     vehicleFrame = new VehicleChassis();
     vehicleType = "None";
     driveTrain = "2WD: Two-Wheel Drive";
-    vehicleEngine = new ManufacturedEngine(); // run the methods to set variables
+    vehicleEngine = new ManufacturedEngine();
   }
 
+  /**
+   * Allows user to create a custom instance by passing in all values relevant to the vehicle and
+   * engine
+   *
+   * @param vehicleManufacturedDate the date the vehicle was manufactured
+   * @param vehicleManufacturer the manufacturer of the vehicle
+   * @param vehicleMake the name of the company who made the vehicle
+   * @param vehicleModel the model name of the vehicle
+   * @param chassisName the name of the chassis used by the vehicle
+   * @param vehicleType the type of vehicle
+   * @param driveTrain the drive train of the vehicle / engine (2 wheel drive, 4 wheel drive, etc.)
+   * @param engineManufacturer the manufacturer of the engine
+   * @param engineManufacturedDate the date the engine was manufactured
+   * @param engineMake the name of the company who made the engine
+   * @param engineModel the model name of the engine
+   * @param engineCylinders the number of cylinders in the engine
+   * @param engineType the type of engine
+   */
   Vehicle(Date vehicleManufacturedDate, String vehicleManufacturer, String vehicleMake,
       String vehicleModel, String chassisName, String vehicleType, String driveTrain,
       String engineManufacturer, Date engineManufacturedDate, String engineMake, String engineModel,
@@ -99,6 +130,14 @@ public class Vehicle implements Engine, Chassis {
     vehicleFrame = new VehicleFrame(chassisType);
   }
 
+  /**
+   * Allows user to print all values in a toString format for both vehicle variables and engine
+   * variables. Since the Engine interface doesn't have any get methods to overload, I was forced to
+   * call the ManufacturedEngine class's toString method to retrieve those variables.
+   *
+   * @return returns all variables related to the vehicle and engine classes related to this
+   * instance.
+   */
   @Override
   public String toString() {
     return "Manufacturer Name   : " + vehicleManufacturer +
@@ -108,6 +147,23 @@ public class Vehicle implements Engine, Chassis {
         "\nVehicleType         : " + vehicleType + "\n" + vehicleEngine.toString();
   }
 
+  /**
+   * Tests the Vehicle class by creating two instances of the Vehicle class: one using the default
+   * constructor and one using the custom info constructor.
+   *
+   * Note: I wasn't sure how to structure the dates based on the output of the step 9 instructions.
+   * The Vehicle instance using the default constructor clearly has the same dates as the Vehicle
+   * instance using the overloaded constructor as far as the month, day, hour, minute, and seconds,
+   * but the year is different. This would be a quick fix for the Vehicle instance using the
+   * overloaded constructor (just modify the year variable from 115 to 112), but then I realized the
+   * default constructor's contents are both different, meaning they couldn't have both used the
+   * new Date() instantiation. The copyright at footer of each page states a date of 2015, so
+   * maybe each 2015 date is meant to be a new Date() instantiation. I'm not really sure what to
+   * make of this.
+   *
+   * @param args arguments received from the command line if the user chosoes to run the program
+   * from there.
+   */
   public static void main(String[] args) {
     Vehicle veh1 = new Vehicle();
     Vehicle veh2 = new Vehicle(new Date(115, 0, 3, 7, 13, 19), "Honda", "Honda", "Prelude",
